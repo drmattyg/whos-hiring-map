e3ba506adfa1e1eed7e8a8b199515c45caee238f
Matthew Gordon <inverarity@gmail.com>
2015-08-31 09:45:04 -0700

    Added Dom handling, untested

--- WHParser.ts
+++ WHParser.ts
@@ -1,16 +1,21 @@
 /// <reference path='typings/htmlparser2/htmlparser2.d.ts' />
 import HtmlParser = require('htmlparser2');
-
+declare var hp;
 class WHParser {
 	html : string;
 	parser: HtmlParser.Parser;
+	handler: HtmlParser.Handler;
 	constructor(html : string) {
 		this.html = html
-		this.parser = new HtmlParser.Parser({
-			onopentag: function(name, attribs) {
-				console.log(name);
-			}
-
+		hp = HtmlParser; // working around some wonkiness with the class declaration here that I don't get
+		this.handler = new hp.DomHandler(function(error, dom) { 
+			this.getJobEntries(dom);
 		});
+		this.parser = new HtmlParser.Parser(this.handler);
+		this.parser.done();
+	}
+
+	getJobEntries(dom) {
+		console.log(dom);
 	}
 }
