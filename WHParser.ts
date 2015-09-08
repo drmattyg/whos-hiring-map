543239805520610c99770ac9a88942472ca354f8
Matthew Gordon <inverarity@gmail.com>
2015-09-07 23:56:24 -0700

    Modified code to get URL and changed htmlparser to cheerio so I can parse more easily

--- WHParser.ts
+++ WHParser.ts
@@ -1,21 +1,14 @@
-/// <reference path='typings/htmlparser2/htmlparser2.d.ts' />
-import HtmlParser = require('htmlparser2');
-declare var hp;
+/// <reference path='typings/cheerio/cheerio.d.ts' />
+import cheerio = require('cheerio');
 class WHParser {
-	html : string;
-	parser: HtmlParser.Parser;
-	handler: HtmlParser.Handler;
-	constructor(html : string) {
+	html: string;
+	//	parser: HtmlParser.Parser;
+	//	handler: HtmlParser.Handler;
+	constructor(html: string) {
 		this.html = html
-		hp = HtmlParser; // working around some wonkiness with the class declaration here that I don't get
-		this.handler = new hp.DomHandler(function(error, dom) { 
-			this.getJobEntries(dom);
-		});
-		this.parser = new HtmlParser.Parser(this.handler);
-		this.parser.done();
-	}
+		var $ = cheerio.load(this.html)
+		$('.c5a').each(function(i, elem) { console.log($(this).text()); });
 
-	getJobEntries(dom) {
-		console.log(dom);
 	}
 }
+export = WHParser;
--- parser.ts
+++ parser.ts
@@ -1,14 +1,10 @@
 /// <reference path='typings/node/node.d.ts' />
-/// <reference path='typings/htmlparser2/htmlparser2.d.ts' />
-
-import http = require('https');
-import parser = require('htmlparser2')
-
-var request = http.request("https://news.ycombinator.com/item?id=9996333");
-request.on('response', function(res) {
-    res.on('data', function(data) {
-        console.log(data.toString());
-    });
+/// <reference path='typings/request/request.d.ts' />
+import WHParser = require('./WHParser');
+import request = require('request');
+var hnUrl : string = 'https://news.ycombinator.com/item?id=9996333';
+request(hnUrl, function(error, response, body) {
+	var whp : WHParser = new WHParser(body)
 });
-request.end();
+//$('.c5a').each(function(i, elem) { console.log($(this).text()); }); 0;
 
