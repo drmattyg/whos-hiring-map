0596e52100c642af73d5a2df4a2eb9eb8c70f247
Matthew Gordon <inverarity@gmail.com>
2015-09-08 17:26:50 -0700

    Added the WHEntry class

--- WHParser.ts
+++ WHParser.ts
@@ -1,14 +1,26 @@
 /// <reference path='typings/cheerio/cheerio.d.ts' />
-import cheerio = require('cheerio');
-class WHParser {
-	html: string;
-	//	parser: HtmlParser.Parser;
-	//	handler: HtmlParser.Handler;
-	constructor(html: string) {
-		this.html = html
-		var $ = cheerio.load(this.html)
-		$('.c5a').each(function(i, elem) { console.log($(this).text()); });
 
+// using https://gist.github.com/kristopolous/19260ae54967c2219da8 as reference for parsing
+
+
+
+//	import cheerio = require('cheerio');
+export class WHEntry { 
+	html: string;
+	header: string;
+	latitude: number;
+	longitude: number;
+}
+export class WHParser {
+	html: string;
+	entries: Array<string>;
+	constructor(html: string) {
+		var cheerio: CheerioAPI = require('cheerio');
+		this.html = html
+		var $ = cheerio.load(this.html);
+		var ent: Array<string> = [];
+		$('.c5a').each(function(i, elem) { ent.push($(this).text().toString()) });
 	}
 }
-export = WHParser;
+//export = WHParser
+
