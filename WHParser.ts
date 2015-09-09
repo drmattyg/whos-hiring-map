f6e6dd243599f93723f0ed69b301815fdd92922c
Matthew Gordon <inverarity@gmail.com>
2015-09-08 17:57:50 -0700

    Made WHEntry parse out the header line Made WHEntry parse out the entry line

--- WHParser.ts
+++ WHParser.ts
@@ -10,16 +10,25 @@ export class WHEntry {
 	header: string;
 	latitude: number;
 	longitude: number;
+	constructor(html: string) {
+		this.html = html;
+		this.header = html.split(/(<p>|\n)/)[0]
+	}
 }
 export class WHParser {
 	html: string;
-	entries: Array<string>;
+	entries: Array<WHEntry>;
 	constructor(html: string) {
 		var cheerio: CheerioAPI = require('cheerio');
 		this.html = html
 		var $ = cheerio.load(this.html);
-		var ent: Array<string> = [];
-		$('.c5a').each(function(i, elem) { ent.push($(this).text().toString()) });
+		var ent: Array<WHEntry> = [];
+		$('.c5a').each(function(i, elem) { 
+			console.log($(this));
+			var entry : WHEntry = new WHEntry($(this).html())
+			ent.push(entry) 
+		});
+		this.entries = ent;
 	}
 }
 //export = WHParser
