829ed8520aec77658bd3d99ed51c999e219f28e4
Matthew Gordon <inverarity@gmail.com>
2015-09-13 19:43:10 -0700

    Removed the test data and inserted the actual processor

--- NERClient.ts
+++ NERClient.ts
@@ -18,22 +18,47 @@ export class NERClient {
 		this.port = port;
 		this.host = host;
 	}
+	foo(bar : string) : string { 
+		return "foo" + bar;
+	}
+	processResults(result: string): Array<NEREntity> {
+		var entities: Array<NEREntity> = result.split(/\s+/).map(function(value: string) {
+			var e = new NEREntity() // debug
+			var m: RegExpMatchArray = value.match(this.reEntString);
+			if (m === null || m.length != 3) { throw "Invalid input from NER" }
+			if (m[2] === "LOCATION") {
+				e.isLocation = true
+			} else {
+				e.isLocation = false
+			}
+			e.name = m[1]
+			return e;
+		});
+		var coalescedEntities: Array<NEREntity> = new Array<NEREntity>();
+		var wasLocation: boolean = false;
+		entities.forEach(function(e: NEREntity, ix: number) {
+			if (ix != 0 && e.isLocation && coalescedEntities[ix - 1].isLocation) {
+				var previous: NEREntity = coalescedEntities.pop();
+				previous.name = previous.name + " " + e.name
+				coalescedEntities.push(previous)
+			} else {
+				coalescedEntities.push(e)
+			}
+		});
+		return coalescedEntities;
+
+	}
 
 	query(text: string, callback: (entities: Array<NEREntity>) => void): void {
 		if (text[text.length - 1] != "\n") { text = text + "\n"; }
 		var client: net.Socket = net.connect({ port: this.port, host: this.host, function() { client.write(text + "\n"); } });
 		var result: string = null;
-		client.on("data", function(data) {
+		client.on("data", (data) => {
 			this.queryResult = data.toString();
 		});
-		client.on("end", function() {
-			// test data here; here's where we'll do the actual data processing
-			var entities: Array<NEREntity> = new Array<NEREntity>();
-			var e: NEREntity = new NEREntity();
-			e.name = "San Francisco";
-			e.isLocation = true;
-			entities.push(e);
-			callback(entities);
+		client.on("end", () => {
+			console.log(this.queryResult);
+			callback(this.processResults(this.queryResult));
 			
 
 		});
@@ -47,32 +72,6 @@ export class NERClient {
 	}
 
 
-	processResults(result: string): Array<NEREntity> {
-		var entities: Array<NEREntity> = result.split(/\s+/).map(function(value: string) {
-			var e = new NEREntity()
-			var m: RegExpMatchArray = value.match(this.reEntString);
-			if (m === null || m.length != 3) { throw "Invalid input from NER" }
-			if (m[2] === "LOCATION") {
-				e.isLocation = true
-			} else {
-				e.isLocation = false
-			}
-			e.name = m[1]
-			return e;
-		});
-		var coalescedEntities: Array<NEREntity> = new Array<NEREntity>();
-		var wasLocation: boolean = false;
-		entities.forEach( function(e : NEREntity, ix: number){
-			if (ix != 0 && e.isLocation  && coalescedEntities[ix - 1].isLocation) {
-				var previous: NEREntity = coalescedEntities.pop();
-				previous.name = previous.name + " " + e.name
-				coalescedEntities.push(previous)
-			} else {
-				coalescedEntities.push(e)
-			}
-		});
-		return coalescedEntities;
 
-	}
 
 }
