7df8d474bbf108cf21fb751943b3dc43ff71e498
Matthew Gordon <inverarity@gmail.com>
2015-09-13 19:13:37 -0700

    Added a test case for the NERClient, it passes nominally but only because I'm just logging output, need to fix the NERCLient

--- test.ts
+++ test.ts
@@ -1,11 +1,24 @@
 /// <reference path="typings/mocha/mocha.d.ts" />
 /// <reference path="typings/assert/assert.d.ts" />
-import assert = require('assert')
-describe("Simple test", () => {
-	describe("Simple test 1", () => {
-		it('Should be really simple', () => {
-			var n: number = 2 + 3;
-			assert.equal(n, 5);
+import assert = require('assert');
+import NERClient = require('./NERClient');
+
+describe("Simple test 1", () => {
+	it('Tests that Grunt/Mocha tests are functioning', () => {
+		var n: number = 2 + 3;
+		assert.equal(n, 5);
+	});
+});
+
+describe("NERClient tests", () => {
+	describe("NERClient simple location test", () => {
+		var testPhrase: string = "I used to live in Chicago but I don't live there anymore"
+		it("Tests the phrase '" + testPhrase + "'", (done) => {
+			var nc: NERClient.NERClient = new NERClient.NERClient(9191, "localhost");
+			nc.query(testPhrase, (entities: Array<NERClient.NEREntity>) => {
+				console.log(entities);
+				done();
+			});
 		});
-	})
+	});
 });
