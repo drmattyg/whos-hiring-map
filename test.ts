e2bb22ae900ad0f165a2b564eabc490f7842c397
Matthew Gordon <inverarity@gmail.com>
2015-09-14 17:32:54 -0700

    Added a real test for coalescing entities

--- test.ts
+++ test.ts
@@ -12,13 +12,32 @@ describe("Simple test 1", () => {
 
 describe("NERClient tests", () => {
 	describe("NERClient simple location test", () => {
-		var testPhrase: string = "I used to live in Chicago but I don't live there anymore"
-		it("Tests the phrase '" + testPhrase + "'", (done) => {
-			var nc: NERClient.NERClient = new NERClient.NERClient(9191, "localhost");
-			nc.query(testPhrase, (entities: Array<NERClient.NEREntity>) => {
-				console.log(entities);
+		var testPhrase1: string = "I used to live in Chicago but I don't live there anymore"
+		var nc: NERClient.NERClient = new NERClient.NERClient(9191, "localhost");	
+		it("Tests the phrase '" + testPhrase1 + "'", (done) => {
+
+			nc.query(testPhrase1, (entities: Array<NERClient.NEREntity>) => {
+				assert.equal(entities.length, 13);
+				assert(
+					entities.filter((e: NERClient.NEREntity) => { return e.name === 'Chicago'})[0].isLocation
+					);
+				assert.equal(entities.filter((e: NERClient.NEREntity) => { return e.name != 'Chicago' && e.isLocation }).length, 0
+					);
 				done();
 			});
+		});
+
+		var testPhrase2: string = "I left my heart in San Francisco CA but my soul is in New Orleans";
+		it("Tests coalescing entities", (done) => { 
+			nc.query(testPhrase2, (entities: Array<NERClient.NEREntity>) => {
+				var locations  : Array<NERClient.NEREntity> = entities.filter((e: NERClient.NEREntity) => { return e.isLocation });
+				assert.equal(locations.length, 2);
+				assert.notEqual(locations.filter((e) => { return e.name == "San Francisco CA" }), -1);
+				assert.notEqual(locations.filter((e) => { return e.name == "New Orleans" }), -1);	
+				done(); 
+			});
+
+
 		});
 	});
 });
